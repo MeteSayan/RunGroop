61cf76890fb2c079e550e9a68c41d8230b15e110
Mete Sayan <metesayan99@gmail.com>
2023-08-13 21:15:39 +0300

    feat: Models Created

---
+++ RunGroopWebApp/Models/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RunGroopWebApp.Models
+{
+    public class AppUser
+    {
+        public int? Pace { get; set; }
+        public int? Mileage { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Clubs { get; set; }
+        public ICollection<Race> Races { get; set; }
+    }
+}

12c8ca642118cf8d3900bd7479414cd988435965
Mete Sayan <metesayan99@gmail.com>
2023-08-25 23:50:43 +0300

    feat: List users and user detail page

--- RunGroopWebApp/Program.cs
+++ RunGroopWebApp/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddScoped<IClubRepository, ClubRepository>();
 builder.Services.AddScoped<IRaceRepository, RaceRepository>();
 builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection(nameof(CloudinarySettings)));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {

580db027991959737120af17ab3f8ff316351a87
Mete Sayan <metesayan99@gmail.com>
2023-08-26 17:22:39 +0300

    Update HomeController.cs

--- RunGroopWebApp/Controllers/HomeController.cs
+++ RunGroopWebApp/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace RunGroopWebApp.Controllers
             var homeViewModel = new HomeViewModel();
             try
             {
-                string url = "https://ipinfo.io?token=" + _config.GetValue<string>("IPInfoToken"); ;
+                string url = "https://ipinfo.io?token=" + _config.GetValue<string>("IPInfoToken");
                 var info = new WebClient().DownloadString(url);
                 ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
                 RegionInfo myRI1 = new RegionInfo(ipInfo.Country);

5bb358ae05849e09a2b3e549c6cace074cd1db5f
Mete Sayan <metesayan99@gmail.com>
2023-08-26 17:03:41 +0300

    feat: Home Page updated with IP Geo Location feature

--- RunGroopWebApp/Controllers/HomeController.cs
+++ RunGroopWebApp/Controllers/HomeController.cs
@@ -1,21 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RunGroopWebApp.Interfaces;
 using RunGroopWebApp.Models;
 using System.Diagnostics;
+using RunGroopWebApp.Helpers;
+using RunGroopWebApp.ViewModels;
+using System.Net;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace RunGroopWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IClubRepository _clubRepository;
+        private readonly IConfiguration _config;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IClubRepository clubRepository, IConfiguration config)
         {
             _logger = logger;
+            _clubRepository = clubRepository;
+            _config = config;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var ipInfo = new IPInfo();
+            var homeViewModel = new HomeViewModel();
+            try
+            {
+                string url = "https://ipinfo.io?token=" + _config.GetValue<string>("IPInfoToken"); ;
+                var info = new WebClient().DownloadString(url);
+                ipInfo = JsonConvert.DeserializeObject<IPInfo>(info);
+                RegionInfo myRI1 = new RegionInfo(ipInfo.Country);
+                ipInfo.Country = myRI1.EnglishName;
+                homeViewModel.City = ipInfo.City;
+                homeViewModel.State = ipInfo.Region;
+                if (homeViewModel.City != null)
+                {
+                    homeViewModel.Clubs = await _clubRepository.GetClubByCity(homeViewModel.City);
+                    if (homeViewModel.Clubs.Count() < 1)
+                    {
+                        homeViewModel.Clubs = null;
+                    }
+                }
+                else
+                {
+                    homeViewModel.Clubs = null;
+                }
+                return View(homeViewModel);
+            }
+            catch (Exception ex)
+            {
+                homeViewModel.Clubs = null;
+            }
+
+            return View(homeViewModel);
         }
 
         public IActionResult Privacy()
